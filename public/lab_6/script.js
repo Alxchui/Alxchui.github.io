e9519e7d6d82ccf4467ab819bc5e802e941aeb1c
Alxchui <65416283+Alxchui@users.noreply.github.com>
2020-10-24 21:43:04 -0400

    lab6 part 2

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -30,6 +30,25 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const unfilledArray = range(10);
+      const filledArray = unfilledArray.map(()=> {
+        const ranNum=Math.floor(Math.random() * 243);
+        return fromServer[ranNum];
+      });
+      const reversedArray=filledArray.sort((a, b) => sortFunction(a, b, key));
+      const ul = document.createElement('ul');
+      ul.className = 'flex-inner';
+      $('form').prepend(ul);
+
+      reversedArray.forEach((element, i) => {
+        const li = document.createElement('li');
+        $(li).append('<input type = "checkbox" value = ${element.code} id = ${element.code} />');
+        $(li).append('<label form$(element.code)>${el.key}</label>');
+        $(ul).append(li);
+      });
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
