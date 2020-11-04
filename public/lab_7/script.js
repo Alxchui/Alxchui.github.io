62784d68387b5fc20d9cbabfab6d224ab4d91a2d
Alxchui <65416283+Alxchui@users.noreply.github.com>
2020-11-04 10:35:19 -0500

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,6 +1,18 @@
 function convertRestaurantsToCategories(restaurantList) {
+  const newDataShape = restaurantList.reduce((collection, item, i) => {
+    const findCat = collection.find((f) => f.label === item.category);
+    if (!findCat) {
+      collection.push({
+        label: item.category,
+        y: 1
+      });
+    } else {
+      findCat.y += 1;
+    }
+    return collection;
+  }, []);
   // process your restaurants here!
-  return list;
+  return newDataShape;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
@@ -14,11 +26,11 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
   ]);
 
-  return {
+  const chart = { 
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Places to eat out in future'
+      text: 'Places To Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -29,7 +41,26 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       gridColor: 'rgba(1,77,101,.1)',
       title: 'Resturants',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {
+        customBreaks: [{
+          startValue: 40,
+          endValue: 50,
+          color: 'orange',
+          type: 'zigzag'
+        },
+        {
+          startValue: 70,
+          endValue: 90,
+          color: 'white',
+          type: 'straight'
+        },
+        {
+          startValue: 135,
+          endValue: 160,
+          color: 'black',
+          type: 'straight'
+        }]
+      } // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
@@ -38,7 +69,7 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       dataPoints: datapointsFromRestaurantsList
     }]
   };
-}
+  return chart; }
 
 function runThisWithResultsFromServer(jsonFromServer) {
   console.log('jsonFromServer', jsonFromServer);
